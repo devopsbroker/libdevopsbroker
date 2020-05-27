287c92faa4969df11b72be880f3499043546feae
Edward Smith <macvelli@gmail.com>
2020-05-26 20:58:15 -0500

    Added functionality for 16-bit data types

---
+++ C/src/org/devopsbroker/lang/short.h
@@ -0,0 +1,58 @@
+/*
+ * short.h - DevOpsBroker C header file for short-related functionality
+ *
+ * Copyright (C) 2020 Edward Smith <edwardsmith@devopsbroker.org>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ * -----------------------------------------------------------------------------
+ * Developed on Ubuntu 18.04.4 LTS running kernel.osrelease = 5.3.0-51
+ *
+ * echo ORG_DEVOPSBROKER_LANG_SHORT | md5sum | cut -c 25-32
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef ORG_DEVOPSBROKER_LANG_SHORT_H
+#define ORG_DEVOPSBROKER_LANG_SHORT_H
+
+// ═════════════════════════════════ Includes ═════════════════════════════════
+
+#include <stdint.h>
+
+#include <assert.h>
+
+// ═══════════════════════════════ Preprocessor ═══════════════════════════════
+
+
+// ═════════════════════════════════ Typedefs ═════════════════════════════════
+
+
+// ═════════════════════════════ Global Variables ═════════════════════════════
+
+
+// ═══════════════════════════ Function Declarations ══════════════════════════
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Utility Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/* ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+ * Function:    a4b6dbea_printShortArray
+ * Description: Prints a short array to stdout
+ *
+ * Parameters:
+ *   shortArray     A pointer to the short array to print
+ *   length         The length of the short array to print
+ * ----------------------------------------------------------------------------
+ */
+void a4b6dbea_printShortArray(uint16_t *shortArray, uint32_t length);
+
+#endif /* ORG_DEVOPSBROKER_LANG_SHORT_H */
